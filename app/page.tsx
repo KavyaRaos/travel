fda6e087dcf9d86b40b9eaf05b9355e824961dca
KavyaRaos <kavyaraos9896@gmail.com>
2024-04-09 15:52:20 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,319 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="mt-8 mx-12">
+      {/* navbar */}
+      <div className="grid grid-flow-col place-content-between">
+        <div className="font-black">Island*</div>
+
+        <div className="grid grid-flow-col place-content-start gap-10">
+          <div className="hover:underline">Our Tours</div>
+          <div className="hover:underline">About Us</div>
+          <div className="hover:underline">Booking</div>
+          <div className="hover:underline">FAQ</div>
+        </div>
+
+        <div>
+          <Image src={"/icons/menu.svg"} alt={""} height={25} width={25} />
+        </div>
+      </div>
+      {/*  */}
+      <div>
+        <div className="relative">
+          <div className="grid grid-flow-row gap-10 absolute ml-16 mt-20 ">
+            <div className="font-black text-8xl text-white ">
+              Explore the sights of the Azores
+            </div>
+            <div className="text-white">
+              A place where nature and adventure unites
+            </div>
+            {/* Book now button */}
+            <div className="text-lg font-medium text-gray-500 bg-white border-2 text-nowrap rounded-full w-min px-5 py-4 hover:text-blue-">
+              Book now
+            </div>
+            {/* Forward and backward button */}
+            <div className="grid grid-flow-col justify-start gap-4">
+              <div>
+                <Image
+                  className=""
+                  src={"/images/whitearrow.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+              <div>
+                <Image
+                  className=""
+                  src={"/images/blackarrow.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+            </div>
+          </div>
+
+          <Image
+            className="my-10 rounded-lg overflow-hidden h-[700px]"
+            src={"/images/Land1.png"}
+            alt={""}
+            height={900}
+            width={2000}
+          />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* part 2 */}
+      <div className="px-10 py-10">
+        <div className="flex flex-col items-center ">
+          <div className="font-bold text-5xl py-5">Top Values for you</div>
+          <div>Try variety of benefits when using our service</div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* 4 cards */}
+      <div className="container mx-auto px-20 py-20">
+        <div className="grid grid-flow-col gap-10 ">
+          {/* card 1 */}
+          <div className="grid grid-flow-row place-items-center text-center">
+            <div>
+              <Image
+                className=""
+                src={"/images/pickup.png"}
+                alt={""}
+                height={50}
+                width={50}
+              />
+            </div>
+            <div className="py-5">
+              <h1 className="font-semibold">Airport booking</h1>
+              <p>We provide escortf rom the airport to the hotel</p>
+            </div>
+          </div>
+          {/* card 2 */}
+          <div className="grid grid-flow-row place-items-center text-center">
+            <div>
+              <Image
+                className=""
+                src={"/images/booking.png"}
+                alt={""}
+                height={50}
+                width={50}
+              />
+            </div>
+            <div className="py-5">
+              <h1 className="font-semibold">Easy booking</h1>
+              <p>Quick and easy booking of tours for upcoming dates</p>
+            </div>
+          </div>
+          {/* card 3 */}
+          <div className="grid grid-flow-row place-items-center text-center">
+            <div>
+              <Image
+                className=" align-center"
+                src={"/images/giude.png"}
+                alt={""}
+                height={50}
+                width={50}
+              />
+            </div>
+            <div className="py-5">
+              <h1 className="font-semibold">Best tour giude</h1>
+              <p>Our best tour guides is ready to guide your trip</p>
+            </div>
+          </div>
+          {/* card 4 */}
+          <div className="grid grid-flow-row place-items-center text-center">
+            <div>
+              <Image
+                className=" align-center"
+                src={"/images/promo.png"}
+                alt={""}
+                height={50}
+                width={50}
+              />
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className="py-5">
+              <h1 className="font-semibold">Lots of promos</h1>
+              <p>Various promotions and drawing of tours</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+
+      {/* Part 3 */}
+      <div className="container mx-auto ">
+        <div className="font-bold text-4xl">
+          <h1>Choose your tour</h1>
+        </div>
+        <div className="grid grid-flow-col gap-10 m-10">
+          {/* card 1 */}
+          <div className=" rounded-lg shadow-lg">
+            <div>
+              <Image
+                className=" rounded-lg"
+                src={"/images/alone.png"}
+                alt={""}
+                height={300}
+                width={300}
+              />
+            </div>
+
+            <div className="grid grid-flow-col items-center">
+              <div className="py-5 px-4">
+                <h1 className="font-semibold">Alone with nature</h1>
+                <div className="grid grid-flow-col justify-start">
+                  <p className="font-semibold">$100</p>
+                  <p>/person</p>
+                </div>
+              </div>
+              {/* card forward icon */}
+              <div>
+                <Image
+                  className=" rounded-lg"
+                  src={"/images/arrow-3.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* card 2 */}
+          <div className=" rounded-lg shadow-lg">
+            <div>
+              <Image
+                className=" rounded-lg"
+                src={"/images/egpt.png"}
+                alt={""}
+                height={300}
+                width={300}
+              />
+            </div>
+
+            <div className="grid grid-flow-col items-center">
+              <div className="py-5 px-4">
+                <h1 className="font-semibold">Egypt</h1>
+                <div className="grid grid-flow-col justify-start">
+                  <p className="font-semibold">$500</p>
+                  <p>/person</p>
+                </div>
+              </div>
+              {/* card forward icon */}
+              <div>
+                <Image
+                  className=" rounded-lg"
+                  src={"/images/arrow-3.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* card 3 */}
+          <div className=" rounded-lg shadow-lg">
+            <div>
+              <Image
+                className=" rounded-lg"
+                src={"/images/jride.png"}
+                alt={""}
+                height={300}
+                width={300}
+              />
+            </div>
+
+            <div className="grid grid-flow-col items-center">
+              <div className="py-5 px-4">
+                <h1 className="font-semibold">Jeep ride</h1>
+                <div className="grid grid-flow-col justify-start">
+                  <p className="font-semibold">$200</p>
+                  <p>/person</p>
+                </div>
+              </div>
+              {/* card forward icon */}
+              <div>
+                <Image
+                  className=" rounded-lg"
+                  src={"/images/arrow-3.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+            </div>
+          </div>
+          {/* card 4 */}
+          <div className=" rounded-lg shadow-lg">
+            <div>
+              <Image
+                className=" rounded-lg"
+                src={"/images/hiking.png"}
+                alt={""}
+                height={300}
+                width={300}
+              />
+            </div>
+
+            <div className="grid grid-flow-col items-center">
+              <div className="py-5 px-4">
+                <h1 className="font-semibold">Hiking</h1>
+                <div className="grid grid-flow-col justify-start">
+                  <p className="font-semibold">$150</p>
+                  <p>/person</p>
+                </div>
+              </div>
+              {/* card forward icon */}
+              <div>
+                <Image
+                  className=" rounded-lg"
+                  src={"/images/arrow-3.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+            </div>
+          </div>
+          {/* card 5 */}
+          <div className=" rounded-lg shadow-lg">
+            <div>
+              <Image
+                className=" rounded-lg"
+                src={"/images/island.png"}
+                alt={""}
+                height={300}
+                width={300}
+              />
+            </div>
+
+            <div className="grid grid-flow-col items-center">
+              <div className="py-5 px-4">
+                <h1 className="font-semibold">Corners of the island</h1>
+                <div className="grid grid-flow-col justify-start">
+                  <p className="font-semibold">$220</p>
+                  <p>/person</p>
+                </div>
+              </div>
+              {/* card forward icon */}
+              <div>
+                <Image
+                  className=" rounded-lg"
+                  src={"/images/arrow-3.png"}
+                  alt={""}
+                  height={30}
+                  width={30}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
